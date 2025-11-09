baa3ca7c0f2545ca44b07ec071f1b1e6e2bbd435
Austin Bao <austin_dun@outlook.com>
2025-11-09 14:00:09 -0700

    added pdf to script and now no longer using hardcoded script

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -7,9 +7,9 @@ function App() {
     <div className="bg-blue-950 min-h-screen w-full">
       <div className="flex flex-col items-center p-6">
         <h1 className="text-3xl font-bold text-indigo-400 mb-6">🎤 Orator AI</h1>
+        <Textbox />
         <div className='mb-6'><Camera /></div>
         <Recorder />
-        <Textbox />
       </div>
     </div>
   );
