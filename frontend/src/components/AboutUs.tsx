7de94d90bf54934be8916c432b4e7a3756093f47
Raphael Ho <raphaeldominique@gmail.com>
2025-11-09 21:32:48 -0700

    Changed Some Descriptions Top Product and Bottom About Us

--- frontend/src/components/AboutUs.tsx
+++ frontend/src/components/AboutUs.tsx
@@ -1,10 +1,18 @@
 import React, { forwardRef } from 'react';
 
 const AboutUs = forwardRef((props, ref) => {
+  const list_styling = "list-disc "
   return (
     <div ref={ref} className="flex flex-col justify-center items-center w-full">
-      <h1 className="text-5xl my-10 text-center w-4/10">About Us</h1>
-      <p className="text-lg">asdasd</p>
+      <h1 className="text-5xl my-10 text-center w-4/10">Features</h1>
+      <h3 className="text-lg">
+        <ul>
+          <li className={list_styling} >Video Detection</li>
+          <li className={list_styling}>Audio Detection</li>
+          <li className={list_styling}>Live AI Helper</li>
+          <li className={list_styling}>Slideshow Presentation</li>
+        </ul>
+      </h3>
     </div>
   );
 });
