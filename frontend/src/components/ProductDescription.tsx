5472e5009b8af9b6966a3df9e204d0cce9e1c4be
davidxia <d.xia111@gmail.com>
2025-11-09 22:59:52 -0700

    Update intro paragraph

--- frontend/src/components/ProductDescription.tsx
+++ frontend/src/components/ProductDescription.tsx
@@ -7,12 +7,7 @@ const ProductDescription = forwardRef((props, ref) =>{
       <h1 className="text-5xl text-center w-4/10">Upgrade your <span className="font-bold">Public Speaking</span> Skills</h1>
       <GetStartedButton>Try Orator Now</GetStartedButton>
      <p className="text-xl text-center"> 
-        Using the Muse S headband, video tracking, and speech patterns, Orator helps you succeed in delivering an explorative and authentic speech while also guiding you along the process.
-        Through EEG data, Alpha, Beta, Theta, and Delta waves can be recorded from the four nodes located near the temple and the forehead. The activation of neurons are picked up 
-        using EEG and the data is transmitted through to our software, which will then help you understand stress levels and emotional wellbeing as you speak. Using video tracking software will
-        observe the movements of the presenter and pop an alert to help you improve engagement with your audience! Gestures that might distract from the presentation of the product will be flagged
-        and the user will be reminded of their behaviour. Not only that, using GeminiApi, we will help you emphasize the main ideas of your presentation in real time! A simple highlight will let us know 
-        where your main points will be, and as they are coming up or if the user forgets, a simple alert will help push them back on the right track.
+        Orator helps you succeed in delivering an explorative and authentic speech using electropalatography, video tracking, and advanced speech-to-text. Using the Muse S headset, Alpha, Beta, Theta, and Delta waves are recorded from the four nodes located near the temple and the forehead. The brain's electrical signals are picked up using EEG and the data is transmitted to our software, which will then help you understand your stress levels and emotional wellbeing as you speak. A live video feed and computer vision model also observes your movements and sends alerts to help you engage more with your audience. If the system detects gestures that distract from the presentation or speech a gentle alert will appear. Additionally, Orator helps you emphasize the main ideas of your presentation in real time using an LLM powered coach. A simple highlight lets us know where your main points are, and an alert notifies you of upcoming or forgotten points.
       </p>
     </div>
   );
